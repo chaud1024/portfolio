c402fe5b1b7dbe51270f25ea58ccd15ed2508c7a
chaud1024 <chaud1024@gmail.com>
2022-06-21 18:04:34 +0900

    slide pagination additonal modify & nav bar added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,24 @@
 import React from 'react'
 
 import Logo from '../assets/images/logo.png';
+import MenuIcon from '../assets/icon/menu.png';
 
 const Navbar = () => {
   return (
     <nav className="nav_bar">
         <img src={Logo} alt="home" />
+
+        <div className="nav_right">
+          <div className="nav_right_lng">
+            <select>
+              <option value="KOR">KOR</option>
+              <option value="ENG">ENG</option>
+            </select>
+          </div>
+          <div className='nav_right_menu'>
+            <img src={MenuIcon} alt="menu icon" />
+          </div>
+        </div>
     </nav>
   )
 }
